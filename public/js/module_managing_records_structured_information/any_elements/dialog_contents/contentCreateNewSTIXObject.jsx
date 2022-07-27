5bb6502ea178010123331caa63e58fc864ea099f
Artemy Belyakov <beljakovav@gmail.com>
2022-07-27 17:21:39 +0300

    ISEMS-UI 27.07.2022

--- public/js/module_managing_records_structured_information/any_elements/dialog_contents/contentCreateNewSTIXObject.jsx
+++ public/js/module_managing_records_structured_information/any_elements/dialog_contents/contentCreateNewSTIXObject.jsx
@@ -241,6 +241,7 @@ export default function CreateDialogContentNewSTIXObject(props){
 
         };
 
+        
     let handlerAddSTIXObject = (elem) => {
 
         console.log("@@@@@@@ func 'handlerAddSTIXObject', elem: ", elem, " @@@@@@");
